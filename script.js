fc5acfcce0ef1e4890cd81efea3406e286126c68
MJOSEGD <mjose.gildelgado@gmail.com>
2025-01-24 10:38:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -508,7 +508,8 @@ class MaterialityMatrix {
     // Calculate adjusted score based on priority range
     const adjustedScore = this.calculateAdjustedValue(score, selectedTopic.priority);
     const adjustedStakeholders = this.calculateAdjustedValue(stakeholders, selectedTopic.priority);
-    
+      // Stakeholders no necesita ajuste (siempre de 0 a 100)
+  const adjustedStakeholders = stakeholders;
     const materialidad = (adjustedScore * 0.65 + adjustedStakeholders * 0.35).toFixed(2);
     
     results.set(selectedTopic.name, {
